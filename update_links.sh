e7cf952ca1f01fc280bc406127885d356d014c8b
piotr.lewicki <piotr.lewicki@elfin.de>
2016-02-15 14:13:15 +0100

    instructions as comments in update_links.sh

--- update_links.sh
+++ update_links.sh
@@ -12,8 +12,16 @@ for f in $FILES; do
     diff $f "$HOME/.$f" -q
     if [ "$exit_status" -eq 1 ]; then
         echo "DBG: Files differ. Name: $f"
+        # backup file
+        # create a symlink
     else if [ "$exit_status" -eq 2 ]; then
         echo "DBG: No such file $HOME/.$f"
+        # output that there is no such file
+        # create a symlink
+    else if [ "$exit_status" -eq 0 ]; then
+        # notify that files are the same
+        # if not symlink -> about to overwrite
+    fi
     fi
     fi
 done
